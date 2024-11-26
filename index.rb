6e8fabe688c0e98d212a64a6474188ac1cfb4a5a
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-11-26 22:29:50 +0000

    Add skeleton

--- index.rb
+++ index.rb
@@ -1,2 +1,8 @@
 #Implement a Caesar cipher that takes in a string and the shift factor, 
-#then outputs the modified string using the right shift.
+#then outputs the modified string using the right shift
+
+def caesar_cipher(string, shift_factor)
+    ...
+end
+
+caesar_cipher("What a string!", 5)
