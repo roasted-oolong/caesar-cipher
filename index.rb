3aa1e5e8ba7b6f53c8f480e42033d526291cfb6b
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-11-27 17:21:41 +0000

    Complete

--- index.rb
+++ index.rb
@@ -2,7 +2,7 @@
 #then outputs the modified string using the right shift
 
 def caesar_cipher(string, shift_factor)
-    ...
+  string.tr('A-Za-z', 'N-ZA-Mn-za-m'.tr('N', ('A'.ord + shift_factor % 26).chr))
 end
 
 caesar_cipher("What a string!", 5)
