7003c72954019415db4aa2bf5f722505c899f9a5
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-12-23 23:20:06 +0000

    Playing w/ RuboCop

--- index.rb
+++ index.rb
@@ -1,8 +1,8 @@
-#Implement a Caesar cipher that takes in a string and the shift factor, 
-#then outputs the modified string using the right shift
+# Implement a Caesar cipher that takes in a string and the shift factor,
+# then outputs the modified string using the right shift
 
 def caesar_cipher(string, shift_factor)
   string.tr('A-Za-z', 'N-ZA-Mn-za-m'.tr('N', ('A'.ord + shift_factor % 26).chr))
 end
 
-caesar_cipher("What a string!", 5)
+caesar_cipher('What a string!', 5)
